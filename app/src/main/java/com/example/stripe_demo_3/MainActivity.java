a6de761cd06f7d71a9191fb53a4f7490e1c0d3f0
Matthew Major <mrmatias1997@gmail.com>
2022-11-07 03:32:44 -0600

    Added custom currency and custom amount options

--- app/src/main/java/com/example/stripe_demo_3/MainActivity.java
+++ app/src/main/java/com/example/stripe_demo_3/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.stripe_demo_3;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -27,6 +31,7 @@ import com.stripe.android.paymentsheet.PaymentSheetResult;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,7 +47,12 @@ public class MainActivity extends AppCompatActivity {
     private String ephemeralKey;
     private String clientSecret;
 
+    private String amount;
+    private String currency;
+
     private Button btn_StartFlow;
+    private Spinner spn_Currency;
+    private EditText et_Amount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_StartFlow = findViewById(R.id.btn_PaymenttFlow);
+        spn_Currency = findViewById(R.id.spn_Currency);
+        et_Amount = findViewById(R.id.et_Amount);
 
         // Stripe Initial Configuration
         PaymentConfiguration.init(this, STRIPE_PUBLISH_KEY);
@@ -57,44 +69,58 @@ public class MainActivity extends AppCompatActivity {
             onPaymentResult(paymentSheetResult);
         });
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currency_codes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spn_Currency.setAdapter(adapter);
+
         btn_StartFlow.setOnClickListener(new View.OnClickListener(){
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view){
-                PaymentFlow();
+
+                DecimalFormat df = new DecimalFormat("0.00");
+                et_Amount.setText(df.format(Double.parseDouble(et_Amount.getText().toString())));
+
+                amount = et_Amount.getText().toString().replace(".", "");
+
+                currency = spn_Currency.getSelectedItem().toString().toLowerCase();
+
+                Toast.makeText(getApplicationContext(), amount + " " + currency, Toast.LENGTH_LONG).show();
+
+                // region Request
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                        "https://api.stripe.com/v1/customers",
+                        new Response.Listener<String>(){
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject json_data = new JSONObject(response);
+                                    customerID = json_data.getString("id");
+                                    Toast.makeText(getApplicationContext(), "Customer ID - " + customerID, Toast.LENGTH_SHORT).show();
+                                    getEphemeralKey(customerID);
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener(){
+                    public void onErrorResponse(VolleyError error){
+
+                    }
+                }){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> headers = new HashMap<>();
+                        headers.put("Authorization", "Bearer " + STRIPE_SECRET_KEY);
+                        return headers;
+                    }
+                };
+                // endregion
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                requestQueue.add(stringRequest);
             }
         });
-
-        // Create Customer
-        StringRequest stringRequest = new StringRequest(Request.Method.POST,
-                "https://api.stripe.com/v1/customers",
-                new Response.Listener<String>(){
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject json_data = new JSONObject(response);
-                            customerID = json_data.getString("id");
-                            Toast.makeText(getApplicationContext(), "Customer ID - " + customerID, Toast.LENGTH_SHORT).show();
-                            getEphemeralKey(customerID);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener(){
-            public void onErrorResponse(VolleyError error){
-
-            }
-        }){
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headers = new HashMap<>();
-                headers.put("Authorization", "Bearer " + STRIPE_SECRET_KEY);
-                return headers;
-            }
-        };
-
-        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
-        requestQueue.add(stringRequest);
 
     }
 
@@ -157,6 +183,7 @@ public class MainActivity extends AppCompatActivity {
                             clientSecret = json_data.getString("client_secret");
                             Toast.makeText(getApplicationContext(), "Client Secret - " + clientSecret, Toast.LENGTH_SHORT).show();
 
+                            PaymentFlow();
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -178,8 +205,8 @@ public class MainActivity extends AppCompatActivity {
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<>();
                 params.put("customer", customerID);
-                params.put("amount", "1099");
-                params.put("currency", "cad");
+                params.put("amount", amount);
+                params.put("currency", currency);
                 params.put("automatic_payment_methods[enabled]", "true");
                 return params;
             }
